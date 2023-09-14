562031125fda82c7aef4fe2101336253f78dbaa7
Маклашев Артем <maklashev_ak@outlook.com>
2023-09-14 22:37:03 +0400

    MainTable edit

--- client/src/layouts/HomePage/components/MainTable.tsx
+++ client/src/layouts/HomePage/components/MainTable.tsx
@@ -1,39 +1,46 @@
 function MainTable() {
     return (
-        <div className="table-responsive">
-            <table className="table table-fluide table-striped table-bordered table-hover">
-                <span className="border border-warning">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Фамилия</th>
-                            <th scope="col">Обращение</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry the Bird</td>
-                            <td ></td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </span>
-            </table>
-        </div>
+      <div className="table-responsive">
+        <div className="container mt-5">
+          <div className="card">
+                    <div className="card-header">
+                        <h1>текущие данные</h1>
+                        </div>
+              <table className="table table-fluide table-striped table-hover table-bordered">
+                {/* <span className="border border-warning"> */}
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Вид гипсокартона</th>
+                    <th scope="col">План</th>
+                    <th scope="col">Факт</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Декоратор ГСП А-УК 12,5-1200-2500</td>
+                    <td>13300</td>
+                    <td>2500</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Декоратор ГСП А-УК 12,5-1200-3000</td>
+                    <td>5000</td>
+                    <td>0</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>РБМ А-ПЛУК 9,5-1200-2500</td>
+                    <td>0</td>
+                    <td>5000</td>
+                  </tr>
+                </tbody>
+                {/* </span> */}
+              </table>
+            </div>
+          </div>
+        </div>      
     );
 }
 export default MainTable;
