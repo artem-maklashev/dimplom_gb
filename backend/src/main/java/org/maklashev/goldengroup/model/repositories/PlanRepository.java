664c42c5cd4941c90df3fdfae6d9e56f211df12a
Artem Maklashev <artem@maklashev.ru>
2023-11-17 15:43:30 +0400

    add Plan

---
+++ backend/src/main/java/org/maklashev/goldengroup/model/repositories/PlanRepository.java
@@ -0,0 +1,14 @@
+package org.maklashev.goldengroup.model.repositories;
+
+import org.maklashev.goldengroup.model.entity.gypsumboard.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PlanRepository extends JpaRepository<Plan, Integer> {
+    @Query("SELECT id FROM Plan WHERE planDate >= :startDate AND planDate < :endDate")
+    List<Integer> findIdsInDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+}
