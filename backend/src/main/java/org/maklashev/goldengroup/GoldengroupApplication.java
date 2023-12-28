6afd4b1ef839beb08a4fe051120192b0fe5eff14
Artem Maklashev <artem@maklashev.ru>
2023-12-28 16:21:45 +0400

    исправлен запуск backend Добавлен коментарий для запуска nginx в Dockerfile

--- backend/src/main/java/org/maklashev/goldengroup/GoldengroupApplication.java
+++ backend/src/main/java/org/maklashev/goldengroup/GoldengroupApplication.java
@@ -10,14 +10,15 @@ import javax.swing.*;
 public class GoldengroupApplication {
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			JFrame frame = new JFrame("Shift Information");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+//		SwingUtilities.invokeLater(() -> {
+//			JFrame frame = new JFrame("Shift Information");
+//			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//			ConfigurableApplicationContext context = SpringApplication.run(GoldengroupApplication.class, args);
+//			ShiftConsoleReader shiftConsoleReader = context.getBean(ShiftConsoleReader.class);
+//			shiftConsoleReader.readAndPrintShifts(frame);
 			ConfigurableApplicationContext context = SpringApplication.run(GoldengroupApplication.class, args);
-			ShiftConsoleReader shiftConsoleReader = context.getBean(ShiftConsoleReader.class);
-			shiftConsoleReader.readAndPrintShifts(frame);
-		});
+//		});
 	}
 
 
